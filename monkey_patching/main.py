d1907a19d92c8907936d9d4378db7649566de600
jackgao <jackgao1234@gmail.com>
2021-05-28 15:10:01 +0800

    add: monkey patching

---
+++ monkey_patching/main.py
@@ -0,0 +1,37 @@
+from poc_code import Model_YOU_CANNOT_MODIFY
+from utils.bcolor import *
+
+
+print_header('==Normal Calling==')
+a_model = Model_YOU_CANNOT_MODIFY()
+a_model.analyze("jack")
+
+
+def profiling(func):
+    def wrapped(*args, **kwargs):
+        print_warn("**start timing")
+        result = func(*args, **kwargs)
+        print_warn("**end timing")
+        return result
+    return wrapped
+
+print_header('==decorated==')
+profiling(a_model.analyze)("jack")
+
+
+print_header('==Monkey Patching==')
+Model_YOU_CANNOT_MODIFY.analyze = profiling(Model_YOU_CANNOT_MODIFY.analyze)  # working on instance method
+c_model = Model_YOU_CANNOT_MODIFY()
+c_model.analyze("Jack again")
+# profiling the instance method without modifying the source code
+
+d_model = Model_YOU_CANNOT_MODIFY()
+d_model.analyze("How about another model?")
+
+
+print_header('==monkey messes up==')
+def monkey_func(self, original_msg):
+    print("ooh ooh eeh ah ah")
+
+Model_YOU_CANNOT_MODIFY.analyze = monkey_func
+d_model.analyze("replaced with monkey_func")
